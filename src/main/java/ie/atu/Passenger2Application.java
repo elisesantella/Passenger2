3e4196cc1996ec8bcd9d782da685a29e5e0b0c29
Elise <g00387170@atu.ie>
2022-12-23 12:09:45 +0000

    Setting project

---
+++ src/main/java/ie/atu/Passenger2Application.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Passenger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Passenger2Application.class, args);
+	}
+
+}
