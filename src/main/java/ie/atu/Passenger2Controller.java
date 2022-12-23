748eca9ac94fd9181bdda08fe16b56f2b8f6f123
Elise <g00387170@atu.ie>
2022-12-23 12:36:58 +0000

    Adding files

---
+++ src/main/java/ie/atu/Passenger2Controller.java
@@ -0,0 +1,29 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/Passenger")
+
+public class Passenger2Controller {
+    Passenger2Service myService;
+
+    public Passenger2Controller(Passenger2Service myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping
+    public List<Passenger2> getPassenger() {
+        return myService.getPassenger();
+    }
+
+    @GetMapping("/{PassengerID}")
+    public Passenger2 getPassenger(@PathVariable String PassengerID) {
+        return myService.getPassenger(PassengerID);
+    }
+}
+
